144947092937e761a0ec3d6dd41904af2a3ca0ac
rivers <93901095+rivers-sh@users.noreply.github.com>
2024-07-23 01:21:44 -0400

    Create rofi_change_wallpaper.sh

---
+++ scripts/developed/rofi_change_wallpaper.sh
@@ -0,0 +1,15 @@
+# .Desktop is in /home/marcial/.local/share/applications
+
+# Directory containing wallpapers
+WALLPAPER_DIR="$HOME/Pictures/Wallpapers"
+
+# Get list of wallpapers in the directory
+wallpapers=$(ls "$WALLPAPER_DIR")
+
+# Display Rofi menu with wallpaper options
+selected_wallpaper=$(echo "$wallpapers" | rofi -dmenu -i -p "")
+
+# Set the selected wallpaper using feh
+if [ -n "$selected_wallpaper" ]; then
+	feh --bg-fill "$WALLPAPER_DIR/$selected_wallpaper"
+fi
